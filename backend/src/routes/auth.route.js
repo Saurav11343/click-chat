b932e79271c00e252e086258ecc8b5ab3a143d04
Saurav Rajput <sauravrajput11343@gmail.com>
2026-07-05 23:19:36 +0530

    added login controller, validation and route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { register } from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
 
 import { validate } from "../middleware/validate.middleware.js";
-import { registerSchema } from "../../validations/auth.validation.js";
+
+import { registerSchema } from "../validations/auth.validation.js";
+import { loginSchema } from "../validations/auth.validation.js";
 
 const router = express.Router();
 
 router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
---
+++ backend/src/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import ENV from "../config/env.js";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, ENV.JWT_SECRET, {
+    expiresIn: ENV.JWT_EXPIRE || "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
+    secure: ENV.NODE_ENV === "production",
+  });
+
+  return token;
+};
